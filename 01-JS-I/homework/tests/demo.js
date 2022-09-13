6edbea5928d90282b80614a5d6058c1b18073cda
EHuillca <SV77474220@idat.edu.pe>
2022-09-12 23:45:46 -0500

    initial commit

---
+++ 01-JS-I/homework/tests/demo.js
@@ -0,0 +1,12 @@
+// console.log("Hola")
+// console.log(3+4*5)
+//console.log(3*5+4**2)
+// var a = 1, b = 2, c = 3;
+// a = b = c
+// console.log(a)
+// console.log(3 == '3')
+// console.log(3 === '3')
+
+const nuevaMultiplicacion = 10 * 4 === 40 ;
+
+const nuevaResta = 10 - 5 === 5;
